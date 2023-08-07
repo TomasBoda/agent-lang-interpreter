d6b6c57ff35d300a5a853eff47b76a38ce610490
= <tominoboda@gmail.com>
2023-08-07 22:22:16 +0200

    feat(error): Added error type names for better debugging

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -6,18 +6,18 @@ import { RuntimeError, ValueType } from "../runtime/runtime.types";
 export class Error {
 
     static lexer(message: string): LexerOutput {
-        return { status: { code: 1, message } } as LexerOutput;
+        return { status: { code: 1, message: `Lexer Error: ${message}` } } as LexerOutput;
     }
 
     static parser(message: string): ParserError {
-        return { type: NodeType.Error, message } as ParserError;
+        return { type: NodeType.Error, message: `Parser Error: ${message}` } as ParserError;
     }
 
     static runtime(message: string): RuntimeError {
-        return { type: ValueType.Error, message } as RuntimeError;
+        return { type: ValueType.Error, message: `Runtime Error: ${message}` } as RuntimeError;
     }
 
     static interpreter(message: string): InterpreterOutput {
-        return { status: { code: 1, message } } as InterpreterOutput;
+        return { status: { code: 1, message: `Interpreter Error: ${message}` } } as InterpreterOutput;
     }
 }
