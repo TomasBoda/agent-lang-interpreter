e0f75cf8e1433210a343261ec7e3b40cfc2e6433
Tomáš Boďa <tominoboda@gmail.com>
2024-01-04 14:26:48 +0100

    test(lexer): Added lexer tests

--- tests/lexer.test.ts
+++ tests/lexer.test.ts
@@ -5,23 +5,31 @@ import { TokenType } from "../src";
 describe("Lexer", () => {
     
     test("should return EOF as only token on empty source code", () => {
-        const tokens = getTokens("");
+        const code = "";
+        const tokens = getTokens(code);
 
         expect(tokens.length).toBe(1);
         expect(tokens[0]).toEqual({ type: TokenType.EOF, value: "EOF", position: { line: 1, character: 1 } });
     });
 
     test("should return EOF as last token on non-empty source code", () => {
-        const tokens = getTokens("agent person 10 {}");
+        const code = "agent person 10 {}";
+        const tokens = getTokens(code);
 
         expect(tokens.length).toBe(6);
         expect(tokens[tokens.length - 1]).toEqual({ type: TokenType.EOF, value: "EOF", position: { line: 1, character: 19 } });
     });
 
     test("should return non-empty array of tokens from non-empty source code", () => {
-        const tokens = getTokens("agent person 10 {}");
+        const code = "agent person 10 {}";
+        const tokens = getTokens(code);
 
         expect(tokens.length).toBe(6);
+    });
+
+    test("should return correct tokens from non-empty source code", () => {
+        const code = "agent person 10 {}";
+        const tokens = getTokens(code);
 
         expect(tokens[0].type).toEqual(TokenType.Agent);
         expect(tokens[1].type).toEqual(TokenType.Identifier);
@@ -29,5 +37,19 @@ describe("Lexer", () => {
         expect(tokens[3].type).toEqual(TokenType.OpenBrace);
         expect(tokens[4].type).toEqual(TokenType.CloseBrace);
         expect(tokens[5].type).toEqual(TokenType.EOF);
+    });
+
+    test("should throw error on unrecognized lexer token", () => {
+        const code = "agent person 10 { const value = @; }";
+
+        expect(() => getTokens(code))
+            .toThrow("Unrecognized character found in source: @");
+    });
+
+    test("should throw error on more than one decimal point in numeric literal", () => {
+        const code = "agent person 10 { const value = 1.2.3; }";
+
+        expect(() => getTokens(code))
+            .toThrow("Number cannot contain more than one decimal point");
     });
 });
